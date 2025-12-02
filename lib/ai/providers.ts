fddd64e9c72b35182fa137aa31a2c4e5683c01ea
双越 <wangfupeng1988@163.com>
2025-12-02 16:38:52 +0800

    feat: deepseek

--- lib/ai/providers.ts
+++ lib/ai/providers.ts
@@ -5,6 +5,7 @@ import {
   wrapLanguageModel,
 } from "ai";
 import { isTestEnvironment } from "../constants";
+import { deepseek } from '@ai-sdk/deepseek';
 
 export const myProvider = isTestEnvironment
   ? (() => {
@@ -24,13 +25,22 @@ export const myProvider = isTestEnvironment
       });
     })()
   : customProvider({
+      // languageModels: {
+      //   "chat-model": gateway.languageModel("xai/grok-2-vision-1212"),
+      //   "chat-model-reasoning": wrapLanguageModel({
+      //     model: gateway.languageModel("xai/grok-3-mini"),
+      //     middleware: extractReasoningMiddleware({ tagName: "think" }),
+      //   }),
+      //   "title-model": gateway.languageModel("xai/grok-2-1212"),
+      //   "artifact-model": gateway.languageModel("xai/grok-2-1212"),
+      // },
       languageModels: {
-        "chat-model": gateway.languageModel("xai/grok-2-vision-1212"),
+        "chat-model": deepseek("deepseek-chat"),
         "chat-model-reasoning": wrapLanguageModel({
-          model: gateway.languageModel("xai/grok-3-mini"),
+          model: deepseek("deepseek-reasoner"),
           middleware: extractReasoningMiddleware({ tagName: "think" }),
         }),
-        "title-model": gateway.languageModel("xai/grok-2-1212"),
-        "artifact-model": gateway.languageModel("xai/grok-2-1212"),
+        "title-model": deepseek("deepseek-chat"),
+        "artifact-model": deepseek("deepseek-chat"),
       },
     });
