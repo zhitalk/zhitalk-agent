26ee6eea859351f9b872bb26d2c48514b7a2cc07
shuibuzhuo <2301140231@qq.com>
2026-04-12 01:10:29 +0800

    style: user message display

--- components/message.tsx
+++ components/message.tsx
@@ -122,7 +122,7 @@ const PurePreviewMessage = ({
                   <div key={key}>
                     <MessageContent
                       className={cn({
-                        "w-fit break-words rounded-2xl px-3 py-2 text-right text-white":
+                        "w-fit break-words rounded-2xl px-3 py-2 text-left text-white":
                           message.role === "user",
                         "bg-transparent px-0 py-0 text-left":
                           message.role === "assistant",
@@ -134,7 +134,11 @@ const PurePreviewMessage = ({
                           : undefined
                       }
                     >
-                      <Response>{sanitizeText(part.text)}</Response>
+                      {message.role === "user" ? (
+                        <p className="whitespace-pre-wrap">{sanitizeText(part.text)}</p>
+                      ) : (
+                        <Response>{sanitizeText(part.text)}</Response>
+                      )}
                     </MessageContent>
                   </div>
                 );
